21ec459c250788c9687db2fde914a61f165891c6
anandprasad <anandreddy0020@gmail.com>
2022-08-18 16:35:25 +0530

    backend of sor

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const menuRoutes = require('./routes/menuRoutes');
+app.use('/', menuRoutes);
+
+const CONNECTION_URL = "mongodb://localhost:27017/Restaurant";
+const PORT = 5000;
+
+mongoose.connect(CONNECTION_URL,{useUnifiedTopology:true},{useNewUrlParser:true},{useFindAndModify:true})
+        .then(() => {
+            app.listen(PORT,()=>{
+                console.log(`server is running on port ${PORT}`);
+            })
+        })
+        .catch((error) => {
+            console.log(error + "Database connection is failed");
+        })
+
+
+        
